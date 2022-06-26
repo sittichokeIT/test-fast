6d5a44ec6632f36d4d7caed43bcb04597687639e
s6204062620046 <s6204062620046@email.kmutnb.ac.th>
2022-06-27 06:04:10 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+app = FastAPI()
+
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+async def test():
+    return {
+        "message": "hello world"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.2", port=8000, log_level="info",reload =True)
